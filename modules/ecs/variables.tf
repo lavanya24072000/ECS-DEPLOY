9d9c26bf37b517f6305487b3c0a8694f82126afd
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-03 00:40:22 +0530

    Update variables.tf

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -2,4 +2,30 @@ variable "env" { type = string }
 variable "vpc_id" { type = string }
 variable "subnets" { type = list(string) }
 variable "cluster_name" { type = string }
+variable "env" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+ 
+variable "security_group_id" {
+  description = "Security group ID"
+  type        = string
+}
+ 
+variable "nginx_image" {
+  description = "Nginx Docker image"
+  type        = string
+}
+ 
+variable "tomcat_image" {
+  description = "Tomcat Docker image"
+  type        = string
+}
+ 
+variable "apache_image" {
+  description = "Apache Docker image"
+  type        = string
+}
+ 
 
+ 
