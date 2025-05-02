e1848922af5a9e2a6c19381466856ec961148eb2
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-03 01:04:42 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -3,6 +3,3 @@ variable "vpc_id" { type = string }
 variable "public_subnet_ids" { type = list(string) }
 variable "alb_name" { type = string }
 variable "target_groups" { type = map(string) }
-variable "subnets"{
-   type = list(string)
-}
