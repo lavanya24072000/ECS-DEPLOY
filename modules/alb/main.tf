760e0c85de06cbb1ff26edfc8acd972e68d09e6c
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-02 23:19:15 +0530

    Create main.tf

---
+++ modules/alb/main.tf
@@ -0,0 +1,77 @@
+resource "aws_lb" "main" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnet_ids
+  tags = { Environment = var.env }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name   = "${var.env}-alb-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "${var.env}-alb-sg" }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default response"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "nginx" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 10
+  action {
+    type             = "forward"
+    target_group_arn = var.target_groups["nginx"]
+  }
+  condition {
+    path_pattern { values = ["/nginx*"] }
+  }
+}
+
+resource "aws_lb_listener_rule" "tomcat" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 20
+  action {
+    type             = "forward"
+    target_group_arn = var.target_groups["tomcat"]
+  }
+  condition {
+    path_pattern { values = ["/tomcat*"] }
+  }
+}
+
+resource "aws_lb_listener_rule" "apache" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 30
+  action {
+    type             = "forward"
+    target_group_arn = var.target_groups["apache"]
+  }
+  condition {
+    path_pattern { values = ["/apache*"] }
+  }
+}
+
