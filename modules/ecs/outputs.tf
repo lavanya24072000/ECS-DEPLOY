e55d2400136aca09f2bd329a4422bd56ae13d3b5
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-03 00:40:34 +0530

    Update outputs.tf

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -6,3 +6,18 @@ output "target_groups" {
   }
 }
 output "cluster_name" { value = aws_ecs_cluster.main.name }
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.this.id
+}
+ 
+output "nginx_service_name" {
+  value = aws_ecs_service.nginx.name
+}
+ 
+output "tomcat_service_name" {
+  value = aws_ecs_service.tomcat.name
+}
+ 
+output "apache_service_name" {
+  value = aws_ecs_service.apache.name
+}
