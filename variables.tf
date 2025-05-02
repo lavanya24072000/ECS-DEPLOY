cdc1eb63f4f89fe08a27e3e43b672f1ca1d9364f
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-02 23:25:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
+
 variable "env" { type = string }
-variable "vpc_id" { type = string }
-variable "subnets" { type = list(string) }
+variable "aws_region" { type = string }
+variable "vpc_cidr" { type = string }
+variable "azs" { type = list(string) }
 variable "cluster_name" { type = string }
+variable "alb_name" { type = string }
+ 
