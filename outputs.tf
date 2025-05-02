0c445dd584057f5f5707bb48ef0eea6acfc2465b
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-02 23:26:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
-output "target_groups" {
-  value = {
-    nginx  = aws_lb_target_group.nginx.arn
-    tomcat = aws_lb_target_group.tomcat.arn
-    apache = aws_lb_target_group.apache.arn
-  }
-}
-output "cluster_name" { value = aws_ecs_cluster.main.name }
+output "vpc_id" { value = module.vpc.vpc_id }
+output "public_subnets" { value = module.vpc.public_subnets }
+output "ecs_cluster" { value = module.ecs.cluster_name }
+output "alb_dns" { value = module.alb.dns_name }
