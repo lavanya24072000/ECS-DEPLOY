99f68f82043ec7e2d0db9da84a967f23b471d770
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-04 00:09:04 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "alb_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
